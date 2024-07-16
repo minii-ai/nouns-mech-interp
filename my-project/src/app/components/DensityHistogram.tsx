3aa56b3a53a806ce9578f1fea34b1fb877a04d69
Sam659 <samplic1@gmail.com>
2024-07-15 22:44:12 -0400

    chart and plot styling

--- my-project/src/app/components/DensityHistogram.tsx
+++ my-project/src/app/components/DensityHistogram.tsx
@@ -14,20 +14,25 @@ const DensityHistogram: React.FC<DensityHistogramProps> = ({ data }) => {
   const svgRef = useRef<SVGSVGElement | null>(null);
   const width = 500;
   const height = 160;
+  const margin = { top: 10, right: 10, bottom: 30, left: 40 };
 
   useEffect(() => {
     const svg = d3
       .select(svgRef.current)
-      .attr("width", width)
-      .attr("height", height)
-      .style("background", "#f0f0f0")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .style("background", "transparent")
       .style("overflow", "visible");
+
+    const chart = svg
+      .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`);
 
     const xScale = d3
       .scaleBand()
       .domain(data.map((d) => d.activation.toString()))
       .range([0, width])
-      .padding(0); // Remove padding to eliminate the space between the y-axis and the first tick
+      .padding(0.1);
 
     const yScale = d3
       .scaleLinear()
@@ -36,68 +41,49 @@ const DensityHistogram: React.FC<DensityHistogramProps> = ({ data }) => {
 
     const xAxis = d3
       .axisBottom(xScale)
-      .ticks(8) // Set the number of ticks for the x-axis
-      .tickSize(-height) // Extend ticks as grid lines
-      .tickFormat((d) => d); // Show tick labels
+      .tickSize(-height)
+      .tickValues(
+        xScale.domain().filter((_, i) => i % Math.ceil(data.length / 8) === 0)
+      );
 
     const yAxis = d3
       .axisLeft(yScale)
-      .ticks(4) // Specify number of ticks for the y-axis
-      .tickSize(-width) // Extend ticks as grid lines
-      .tickFormat(d3.format(".0f")); // Format the ticks as integers
+      .ticks(3)
+      .tickSize(-width)
+      .tickFormat(d3.format(".0f"));
 
-    // Add X grid lines
-    svg
+    // Add X grid lines and axis
+    chart
       .append("g")
       .attr("class", "grid")
       .attr("transform", `translate(0, ${height})`)
       .call(xAxis)
-      .selectAll(".tick line")
-      .attr("stroke", "#ccc");
+      .call((g) => g.select(".domain").remove()) // Remove the axis line
+      .call((g) => g.selectAll("line").attr("stroke", "#ccc"))
+      .call((g) => g.selectAll("text").attr("fill", "#6b7280"));
 
-    // Add Y grid lines
-    svg
+    // Add Y grid lines and axis
+    chart
       .append("g")
       .attr("class", "grid")
       .call(yAxis)
-      .selectAll(".tick line")
-      .attr("stroke", "#ccc");
-
-    // Remove the axis lines
-    svg.selectAll(".domain").remove();
+      .call((g) => g.select(".domain").remove()) // Remove the axis line
+      .call((g) => g.selectAll("line").attr("stroke", "#ccc"))
+      .call((g) => g.selectAll("text").attr("fill", "#6b7280"));
 
     // Add bars
-    svg
+    chart
       .selectAll(".bar")
       .data(data)
       .enter()
       .append("rect")
       .attr("class", "bar")
-      .attr(
-        "x",
-        (d) =>
-          (xScale(d.activation.toString()) as number) + xScale.bandwidth() / 2
-      )
+      .attr("x", (d) => xScale(d.activation.toString()) as number)
       .attr("y", (d) => yScale(d.count))
       .attr("width", xScale.bandwidth())
       .attr("height", (d) => height - yScale(d.count))
       .attr("fill", "brown");
-
-    // Add gray text labels for the X axis
-    svg
-      .append("g")
-      .attr("transform", `translate(0, ${height})`)
-      .call(d3.axisBottom(xScale).tickSize(0))
-      .selectAll("text")
-      .attr("fill", "gray");
-
-    // Add gray text labels for the Y axis
-    svg
-      .append("g")
-      .call(d3.axisLeft(yScale).ticks(4).tickSize(0))
-      .selectAll("text")
-      .attr("fill", "gray");
-  }, [data, height, width]);
+  }, [data]);
 
   return <svg ref={svgRef}></svg>;
 };
