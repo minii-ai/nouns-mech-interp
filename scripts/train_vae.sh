0b8c73ded8647beb75325e2554ad38fbeab53ace
Eric Chen <eric.chen@backflip.ai>
2024-07-17 02:53:10 +0000

    best vae trained

--- scripts/train_vae.sh
+++ scripts/train_vae.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 python train_vae.py \
-    --num_hiddens=32,64,128,256 \
+    --num_hiddens=64,128,256,512 \
     --latent_dim=4 \
     --lr=1e-3 \
-    --iterations=500000 \
+    --iterations=1000000 \
     --batch_size=32 \
     --max_beta=0.000001 \
-    --save_dir="../checkpoints/vae_new_mse" \
-    --checkpoint_every=2500 \
+    --save_dir="../checkpoints/vae_big" \
+    --checkpoint_every=5000 \
+    # --num_hiddens=32,64,128,256 \
