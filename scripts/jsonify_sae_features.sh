b53877448ed0f4d5209591b4760cd6d80126a2de
Eric Chen <eric.chen@backflip.ai>
2024-07-18 06:28:28 +0000

    jsonifying features

--- scripts/jsonify_sae_features.sh
+++ scripts/jsonify_sae_features.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 python3 jsonify_sae_features.py \
-    --sae_checkpoint="../checkpoints/sae_experiments/lr=5.0e-04_l1=5.0e-03" \
+    --sae_checkpoint="../weights/sae" \
     --output_path="../data/features.json" \
-    --vae_embeddings_path="../vae_embeddings.npy" \
+    --vae_embeddings_path="../data/vae_embeddings.npy" \
     --k=9 \
+    # --sae_checkpoint="../checkpoints/sae_experiments/lr=5.0e-04_l1=5.0e-03" \
